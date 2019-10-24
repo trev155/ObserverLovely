6f2564d33d7c084faca7fe2428df36645b99166d
trev155 <trevku155@gmail.com>
2019-10-23 20:41:13 -0400

    Basic Player Movement and Camera following Player Unit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public PlayerUnit playerUnit;
+
+    private void Update() {
+        Vector3 newPosition = new Vector3(playerUnit.transform.position.x, playerUnit.transform.position.y + 3, -10);
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerUnit : MonoBehaviour {
+    private readonly float MOVE_DISTANCE = 1.0f;
+    private readonly float MOVE_SPEED = 1.0f;
+
+    private void Update() {
+        bool up = Input.GetAxisRaw("Vertical") > 0;
+        bool down = Input.GetAxisRaw("Vertical") < 0;
+        bool left = Input.GetAxisRaw("Horizontal") < 0;
+        bool right = Input.GetAxisRaw("Horizontal") > 0;
+
+        if (up) {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y + MOVE_DISTANCE), Time.deltaTime * MOVE_SPEED);
+        }
+        if (down) {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y - MOVE_DISTANCE), Time.deltaTime * MOVE_SPEED);
+        }
+        if (left) {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x - MOVE_DISTANCE, transform.position.y), Time.deltaTime * MOVE_SPEED);
+        }
+        if (right) {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + MOVE_DISTANCE, transform.position.y), Time.deltaTime * MOVE_SPEED);
+        }
+    }
+}
