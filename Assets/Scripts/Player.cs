9cf4c7697768048d0348f4b75482cebcd315e3ac
trev155 <trevku155@gmail.com>
2019-11-03 14:47:47 -0500

    player reaches the end zone

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,11 +14,16 @@ public class Player : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Observer") {
             CollidedWithObserver();
+        } else if (collision.gameObject.tag == "EndZone") {
+            transform.position = respawnPosition.position;
+            playerMovement.SetIsMoving(false);
+
+            // TODO advance the level
         }
     }
 
     /*
-     * Same as the enter
+     * Same as the enter, but handles the case where you are already touching the observer initially.
      */
     private void OnTriggerStay2D(Collider2D collision) {
         if (collision.gameObject.tag == "Observer") {
@@ -35,7 +40,6 @@ public class Player : MonoBehaviour {
 
     private bool IsInSafeZone() {
         Collider2D playerCollider = GetComponent<Collider2D>();
-        Debug.Log(playerCollider);
         GameObject[] safeZones = GameObject.FindGameObjectsWithTag("SafeZone");
         foreach (GameObject safeZone in safeZones) {
             Collider2D safeZoneCollider = safeZone.GetComponent<Collider2D>();
