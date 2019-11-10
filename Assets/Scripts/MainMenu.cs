8c34234be897a9504d20408ccf0f075c7646ec4c
trev155 <trevku155@gmail.com>
2019-11-10 11:29:17 -0500

    Main Menu - Difficuly selection indicate which button pressed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,30 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
- 
+using UnityEngine.EventSystems;
+
 /*
  * Handles main menu canvas actions.
  */
 public class MainMenu : MonoBehaviour {
-    private GameDifficulty difficultySelection;    // 1 = easy, 2 = medium, 3 = hard
+    public Button easyButton;
+    public Button normalButton;
+    public Button hardButton;
 
-    private void Awake() {
-        difficultySelection = GameDifficulty.NORMAL;    // default
+    private GameDifficulty difficultySelection;
+    
+    private void Start() {
+        DefaultSelection();
+    } 
+
+    private void DefaultSelection() {
+        difficultySelection = GameDifficulty.NORMAL;
+        var pointer = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(normalButton.gameObject, pointer, ExecuteEvents.pointerDownHandler);
+        ExecuteEvents.Execute(normalButton.gameObject, pointer, ExecuteEvents.pointerUpHandler);
+        
     }
 
     public void EasyButton() {
-        difficultySelection = GameDifficulty.EASY;
+        SceneDataTransfer.CurrentGameDifficulty = GameDifficulty.EASY;
     }
 
-    public void MediumButton() {
-        difficultySelection = GameDifficulty.NORMAL;
+    public void NormalButton() {
+        SceneDataTransfer.CurrentGameDifficulty = GameDifficulty.NORMAL;
     }
 
     public void HardButton() {
-        difficultySelection = GameDifficulty.HARD;
+        SceneDataTransfer.CurrentGameDifficulty = GameDifficulty.HARD;
     }
 
     public void StartGame() {
-        SceneDataTransfer.CurrentGameDifficulty = difficultySelection;
         SceneManager.LoadScene("GameField");
     }
 }
--- Assets/Scripts/SceneDataTransfer.cs
+++ Assets/Scripts/SceneDataTransfer.cs
@@ -2,5 +2,5 @@
  * Retain data when switching scenes.
  */
 public static class SceneDataTransfer {
-    public static GameDifficulty CurrentGameDifficulty { get; set; } = GameDifficulty.HARD;
+    public static GameDifficulty CurrentGameDifficulty { get; set; }
 }
